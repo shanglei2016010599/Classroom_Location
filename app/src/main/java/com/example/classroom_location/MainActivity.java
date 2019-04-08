c27f23cc7fdea1481d53c5fc1b9a6ec116263292
shanglei2016010599 <1014952588@qq.com>
2019-04-08 09:44:53 +0800

    实现底部按钮控制两个碎片

--- app/src/main/java/com/example/classroom_location/MainActivity.java
+++ app/src/main/java/com/example/classroom_location/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.classroom_location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +17,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_classroom:
+                    replaceFragment(new ClassroomFragment());
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_forum:
+                    replaceFragment(new ForumFragment());
                     return true;
             }
             return false;
@@ -36,9 +33,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        replaceFragment(new ClassroomFragment());
+    }
+
+    /* 动态添加碎片 */
+    /*
+        创建待添加的碎片实例
+        获取FragmentManager，在获得中可以直接通过调用getSupportFragmentManager()方法得到
+        开启一个事务，通过调用beginTransaction()方法开启
+        向容器内添加或替换碎片，一般使用replace()方法实现，需要传入容器的id和待添加的碎片实例
+        提交事务，调用commit()方法来完成。
+     */
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.commit();
     }
 
 }
