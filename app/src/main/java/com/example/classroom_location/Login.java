edd3eb6c0a6c22a777bc1c91da2f042699db480c
shanglei2016010599 <1014952588@qq.com>
2019-06-11 11:08:23 +0800

    实现访问服务器登录

--- app/src/main/java/com/example/classroom_location/Login.java
+++ app/src/main/java/com/example/classroom_location/Login.java
@@ -3,15 +3,22 @@ package com.example.classroom_location;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 @SuppressLint("Registered")
 public class Login extends AppCompatActivity implements View.OnClickListener{
@@ -20,8 +27,11 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
     private EditText account_edit;
     private EditText password_edit;
     private SharedPreferences preferences;
-    private final static String mAccount = "2016010599";
-    private final static String mPassword = "123456";
+//    private final static String mAccount = "2016010599";
+//    private final static String mPassword = "123456";
+    private final static String TAG = "LoginActivity";
+    String account;
+    String password;
 
     //    private final String TAG = "LoginActivity";
 
@@ -37,6 +47,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         account_edit = findViewById(R.id.account_edit);
         password_edit = findViewById(R.id.password_edit);
         Button logIn = findViewById(R.id.Login_btn);
+
         boolean isRemember = preferences.getBoolean("remember_password", false);
         boolean autoLogin = preferences.getBoolean("isAuto", false);
         if (isRemember){
@@ -51,37 +62,98 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.Login_btn) {
-            String account = account_edit.getText().toString();
-            String password = password_edit.getText().toString();
-            if (account.equals(mAccount) && password.equals(mPassword)) {
-                SharedPreferences.Editor editor = preferences.edit();
-                if (RememberPass.isChecked()) {    //检查复选框是否被选中
-                    editor.putBoolean("remember_password", true);
-                    editor.putString("account", account);
-                    editor.putString("password", password);
-                    editor.putBoolean("isAuto", true);
-                } else {
-                    editor.clear();
+            account = account_edit.getText().toString();
+            password = password_edit.getText().toString();
+            HttpUtil.sendOkHttpRequest("http://192.168.0.101:8080/test1_war_exploded/LoginServlet?" +
+                    "account=" + account +
+                    "&password=" + password, new okhttp3.Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Looper.prepare();
+                    Toast.makeText(Login.this, "访问网络失败，请重试",
+                            Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                    Looper.loop();
                 }
-                editor.apply();
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
-            } else {
-                Toast.makeText(Login.this, "账号或密码错误",
-                        Toast.LENGTH_SHORT).show();
-            }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    assert response.body() != null;
+                    if (response.body().string().equals("success")){
+                        Looper.prepare();
+                        Toast.makeText(Login.this, "登录成功",
+                                Toast.LENGTH_SHORT).show();
+                        SharedPreferences.Editor editor = preferences.edit();
+                        if (RememberPass.isChecked()) {    //检查复选框是否被选中
+                            editor.putBoolean("remember_password", true);
+                            editor.putString("account", account);
+                            editor.putString("password", password);
+                            editor.putBoolean("isAuto", true);
+                        } else {
+                            editor.clear();
+                        }
+                        editor.apply();
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                        Looper.loop();
+                    } else if (response.body().string().equals("fail")){
+                        Toast.makeText(Login.this, "账号或密码错误",
+                                Toast.LENGTH_SHORT).show();
+                        Looper.loop();
+                    }
+
+                }
+            });
+//            if (account.equals(mAccount) && password.equals(mPassword)) {
+//                SharedPreferences.Editor editor = preferences.edit();
+//                if (RememberPass.isChecked()) {    //检查复选框是否被选中
+//                    editor.putBoolean("remember_password", true);
+//                    editor.putString("account", account);
+//                    editor.putString("password", password);
+//                    editor.putBoolean("isAuto", true);
+//                } else {
+//                    editor.clear();
+//                }
+//                editor.apply();
+//                Intent intent = new Intent(Login.this, MainActivity.class);
+//                startActivity(intent);
+//            } else {
+//                Toast.makeText(Login.this, "账号或密码错误",
+//                        Toast.LENGTH_SHORT).show();
+//            }
         }
     }
 
     private void AutoLogin(){
         String account = preferences.getString("account", "");
         String password = preferences.getString("password", "");
-        if (account != null && password != null){
-            if (account.equals(mAccount) && password.equals(mPassword)){
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+        HttpUtil.sendOkHttpRequest("http://192.168.0.101:8080/test1_war_exploded/LoginServlet?" +
+                "account=" + account +
+                "&password=" + password, new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Looper.prepare();
+                Toast.makeText(Login.this, "访问网络失败，请重试",
+                        Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+                Looper.loop();
             }
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                assert response.body() != null;
+                if (response.body().string().equals("success")){
+                    Log.d(TAG, "onResponse: 自动登录成功");
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                } else if (response.body().string().equals("fail")){
+                    Looper.prepare();
+                    Toast.makeText(Login.this, "账号或密码错误",
+                            Toast.LENGTH_SHORT).show();
+                    Looper.loop();
+                }
+            }
+        });
     }
 
     private void init(){
@@ -92,6 +164,5 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         password_edit.setText(password);
         RememberPass.setChecked(true);
     }
-
 
 }
---
+++ app/src/main/java/com/example/classroom_location/User.java
@@ -0,0 +1,65 @@
+package com.example.classroom_location;
+
+public class User {
+
+    private String id;
+
+    private String name;
+
+    private String url;
+
+    private String location;
+
+    private String message;
+
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
